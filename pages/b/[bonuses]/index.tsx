a5a848e2ca70f5ff5b6f2398783a32e1e51d1219
davidevaliante <dav.valiante@gmail.com>
2023-06-22 15:23:45 +0200

    added geolocation

--- pages/b/[bonuses]/index.tsx
+++ pages/b/[bonuses]/index.tsx
@@ -16,14 +16,72 @@ interface Props {
   bonusToShow: number[];
 }
 
+const countryToHeader = (country: string) => {
+  switch (country) {
+    case "it":
+      return "COMPARAZIONE OFFERTE SITI LEGALI IN ITALIA";
+
+    case "row":
+      return "BEST LICENSED CASINOS IN YOUR COUNTRY";
+
+    case "mt":
+      return "BEST LICENSED CASINOS IN MALTA";
+
+    case "nz":
+      return "BEST LICENSED CASINOS IN NEW ZELAND";
+
+    case "ca":
+      return "BEST LICENSED CASINOS IN CANADA";
+
+    case "fi":
+      return "SUOMEN PARHAAT ONLINE-KASINOT";
+
+    case "nz":
+      return "BEST LICENSED CASINOS IN NEW ZEALAND";
+
+    case "au":
+      return "BEST LICENSED CASINOS IN AUSTRALIA";
+
+    case "no":
+      return "BESTE NETTKASINOER I NORGE";
+
+    case "ie":
+      return "BEST LICENSED CASINOS IN IRELAND";
+
+    case "in":
+      return "BEST LICENSED CASINOS IN INDIA";
+
+    case "jp":
+      return "日本人向けのおすすめオンラインカジノ";
+
+    case "es":
+      return "MEJORES CASINOS ONLINE EN ESPAÑA";
+
+    case "de":
+      return "BESTE ONLINE CASINOS IN DEUTSCHLAND";
+
+    case "gb":
+      return "BEST ONLINE CASINOS IN THE UNITED KINGDOM";
+
+    default:
+      return "BEST LICENSED CASINOS IN YOUR COUNTRY";
+  }
+};
+
 const Compare: FunctionComponent<Props> = ({ streamerData, bonusToShow }) => {
   const [country, setCountry] = useState<string | undefined>(undefined);
-
   useEffect(() => {
-    if (country !== "") getBonusByName();
+    if (country !== undefined) {
+      if (country === "it") {
+        getBonusByName();
+      } else {
+        getBonusList();
+      }
+    }
   }, [country]);
+
   const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
-    undefined,
+    undefined
   );
 
   useEffect(() => {
@@ -31,20 +89,73 @@ const Compare: FunctionComponent<Props> = ({ streamerData, bonusToShow }) => {
   }, []);
 
   const geoLocate = async () => {
-    setCountry(undefined);
+    const userCountryRequest = await axios.get(configuration.geoApi);
+    const countryCode = lowerCase(userCountryRequest.data.country_code2);
+    setCountry(countryCode);
+  };
 
-    const offData = await axios.get(
-      "https://spike-2481d.firebaseio.com/Spare/Cobra.json",
+  const getBonusList = async () => {
+    console.log(streamerData);
+    let base = streamerData.countryBonusList.filter(
+      (it) => it.label === country
     );
-    const { active } = offData.data;
 
-    if (!active) {
-      window.location.replace(`/about`);
-    } else {
-      const userCountryRequest = await axios.get(configuration.geoApi);
-      const countryCode = lowerCase(userCountryRequest.data.country_code2);
-      setCountry(countryCode);
+    if (base.length == 0) {
+      setCountry("row");
+      return;
     }
+
+    let bonusForCountry = base[0].bonuses;
+
+    console.log(streamerData.countryBonusList, country);
+
+    // if (bonusForCountry.length == 0) {
+    // 	bonusForCountry = streamerData.countryBonusList.filter(
+    // 		it => it.label === 'row'
+    // 	)[0].bonuses
+    // 	setCountry('row')
+    // }
+
+    const ordering = streamerData.countryBonusList
+      .filter((it) => it.label === country)[0]
+      //@ts-ignore
+      .ordering.split(" ");
+
+    // const requests = bonusForCountry[0].bonuses.map((b) =>
+    //   axios.get(`${configuration.api}/bonuses/${b.id}`)
+    // );
+
+    // const bList = await Promise.all(requests);
+    // let unorderedBonuses = bList.map((r) => r.data as StreamerBonus);
+
+    let unorderedBonuses = [...bonusForCountry];
+
+    let ordered: StreamerBonus[] = [];
+
+    ordering.forEach((code) => {
+      const matchingBonus = unorderedBonuses.find(
+        (it) => it.compareCode === code
+      );
+      if (matchingBonus) {
+        ordered.push(matchingBonus);
+        unorderedBonuses = unorderedBonuses.filter(
+          (b) => b.compareCode !== code
+        );
+      }
+    });
+    console.log(streamerData.bonuses);
+    const finalList = [...ordered]
+      .map((b) => {
+        const match = streamerData.bonuses.find((it) => it.id == b.id);
+        if (match !== undefined) return match;
+        else return undefined;
+      })
+      .filter((it) => it !== undefined);
+
+    console.log(finalList, "final list");
+
+    //@ts-ignore
+    setBonuses(finalList);
   };
 
   const getBonusByName = () => {
@@ -53,125 +164,99 @@ const Compare: FunctionComponent<Props> = ({ streamerData, bonusToShow }) => {
 
     console.log(bonusToShow);
 
-    bonusToShow.forEach((bonusCode) => {
+    const remappedBonusToShow = [...bonusToShow];
+
+    if (remappedBonusToShow.includes(47)) {
+      const indexToRemove = remappedBonusToShow.indexOf(47);
+
+      if (!remappedBonusToShow.includes(55)) {
+        remappedBonusToShow.splice(indexToRemove, 1, 55);
+      } else {
+        remappedBonusToShow.splice(indexToRemove, 1, 33);
+      }
+    }
+
+    remappedBonusToShow.forEach((bonusCode) => {
       const b = streamerBonuses.find((b) => b.id === bonusCode);
       if (b) placeholder.push(b);
     });
 
     setBonuses(placeholder);
-    console.log(placeholder, "bonus to show");
   };
 
-  if (!country) return <FullPageLoader />;
-  if (country !== "it" && country !== "mt") {
-    // if (country === 'it')
-    return (
-      <Wrapper>
-        <Container>
-          <div className="top-bar" style={{ cursor: "pointer" }}>
-            <img className="logo" src="/icons/app_icon.png" />
-          </div>
+  const openWebsite = () => window.open("https://www.spikeslot.com");
 
-          <div>
-            <h1 style={{ margin: "1rem 0rem" }}>
-              Watch more YouTube videos :
-            </h1>
-            <div className="rwd-video">
-              <iframe
-                src={`https://www.youtube.com/embed/290b2Pmj0ZI`}
-                allowFullScreen
-                frameBorder="0"
-                height="315"
-                width="560"
-              />
-            </div>
-
-            <div className="rwd-video">
-              <iframe
-                src={`https://www.youtube.com/embed/NXZsfSlZvL0`}
-                allowFullScreen
-                frameBorder="0"
-                height="315"
-                width="560"
-              />
-            </div>
-
-            <div className="rwd-video">
-              <iframe
-                src={`https://www.youtube.com/embed/X4wJDs8A5lQ`}
-                allowFullScreen
-                frameBorder="0"
-                height="315"
-                width="560"
-              />
-            </div>
-          </div>
-        </Container>
-      </Wrapper>
-    );
-  }
+  if (!bonuses && !country) return <FullPageLoader />;
   return (
     <Wrapper>
       <Container>
-        <div className="top-bar" style={{ cursor: "pointer" }}>
+        <div
+          className="top-bar"
+          style={{ cursor: "pointer" }}
+          onClick={() => openWebsite()}
+        >
           <img className="logo" src="/icons/app_icon.png" />
         </div>
 
-        <h1>Comparazione offerte siti legali in Italia:</h1>
+        {country === "it" ? (
+          <h1>Comparazione offerte siti legali in Italia:</h1>
+        ) : (
+            <h1>Best casino offers for your country</h1>
+          )}
+
+        <div
+          style={{
+            position: "fixed",
+            bottom: "0",
+            left: "0",
+            backgroundColor: "#333",
+            width: "100%",
+            padding: "10px 0px",
+            zIndex: 100,
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              fontSize: ".8rem",
+              textAlign: "center",
+            }}
+          >
+            {countryToHeader(country ? country : "row")}
+          </div>
+        </div>
 
         {bonuses &&
-          bonuses.length > 2 &&
           bonuses.map((bonus: StreamerBonus) => (
             <BonusStripe
               key={`${bonus.name}`}
               bonus={bonus}
-              countryCode={country}
-            />
-          ))}
-
-        {bonuses &&
-          bonuses.length <= 2 &&
-          streamerData.bonuses.map((bonus: StreamerBonus) => (
-            <BonusStripe
-              key={`${bonus.name}`}
-              bonus={bonus}
-              countryCode={country}
+              countryCode={country ? country : "row"}
             />
           ))}
 
         <div style={{ padding: "1rem" }}>
-          <VideoDiscalimer />
+          {country === "it" ? <VideoDiscalimer /> : <div></div>}
         </div>
-        {process.env.REFER === "true" && (
-          <div className="bottom">
-            <p style={{ textAlign: "center" }}>
-              This service is provided by{" "}
-              <a href="https://www.topaffiliation.com">
-                Top Affiliation
-              </a>
-            </p>
-          </div>
-        )}
       </Container>
     </Wrapper>
   );
 };
 
 export async function getServerSideProps({ query }) {
-  const pickedBonus = query.bonuses;
+  const pickedbonus = query.bonuses;
 
-  const aquaClient = new AquaClient();
-
-  const bonusToShow = pickedBonus.split("-").map((b) => parseInt(b));
+  const bonustoshow = pickedbonus.split("-").map((b) => parseInt(b));
 
   const streamer = await axios.get(
-    `${configuration.api}/streamers/${configuration.streamerId}`,
+    `${configuration.api}/streamers/${configuration.streamerId}`
   );
 
   return {
     props: {
       streamerData: streamer.data as Streamer,
-      bonusToShow: bonusToShow,
+      bonusToShow: bonustoshow,
     },
   };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,17 +18,12 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
   const [loading, setLoading] = useState(true);
   const [country, setCountry] = useState<string>("");
   useEffect(() => {
-    if (country === "it") getBonusList();
-    else setLoading(false);
+    country !== "" && getBonusList();
   }, [country]);
   const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
-    undefined,
+    undefined
   );
-  useEffect(() => {
-    console.log(bonuses);
-  }, [bonuses]);
-
-  console.log(streamerData);
+  useEffect(() => { }, [bonuses]);
 
   useEffect(() => {
     geoLocate();
@@ -38,7 +33,7 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
     setLoading(true);
 
     const offData = await axios.get(
-      "https://spike-2481d.firebaseio.com/Spare/Cobra.json",
+      "https://spike-2481d.firebaseio.com/Spare/Cobra.json"
     );
     const { active } = offData.data;
 
@@ -47,21 +42,21 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
     } else {
       const userCountryRequest = await axios.get(configuration.geoApi);
       const countryCode = lowerCase(userCountryRequest.data.country_code2);
+
       if (countryCode) setCountry(countryCode);
-      setLoading(false);
     }
   };
 
   const getBonusList = async () => {
+    console.log(country);
+    console.log(streamerData);
     let bonusForCountry = streamerData.countryBonusList.filter(
-      (it) => it.label === country,
+      (it) => it.label === country
     )[0].bonuses;
-
-    console.log(bonusForCountry, country);
 
     if (bonusForCountry.length == 0) {
       bonusForCountry = streamerData.countryBonusList.filter(
-        (it) => it.label === "row",
+        (it) => it.label === "row"
       )[0].bonuses;
       setCountry("row");
     }
@@ -77,16 +72,16 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
 
     ordering.forEach((code) => {
       const matchingBonus = unorderedBonuses.find(
-        (it) => it.compareCode === code,
+        (it) => it.compareCode === code
       );
       if (matchingBonus) {
         ordered.push(matchingBonus);
         unorderedBonuses = unorderedBonuses.filter(
-          (b) => b.compareCode !== code,
+          (b) => b.compareCode !== code
         );
       }
     });
-    console.log(streamerData.bonuses);
+
     const finalList = [...ordered, ...unorderedBonuses]
       .map((b) => {
         const match = streamerData.bonuses.find((it) => it.id == b.id);
@@ -94,8 +89,6 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
         else return undefined;
       })
       .filter((it) => it !== undefined);
-
-    console.log(finalList, "final list");
 
     //@ts-ignore
     setBonuses(finalList);
@@ -110,21 +103,13 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
           <img className="logo" src="/icons/app_icon.png" />
         </div>
 
-        {country === "it" && (
+        {country === "it" ? (
           <h1>Comparazione offerte siti legali in Italia:</h1>
-        )}
-
-        {country !== "it" && (
-          <iframe
-            style={{ margin: "0 auto", display: "block" }}
-            width="100%"
-            height="500px"
-            src="https://www.youtube.com/embed/z2AbJd3Zxe4"
-          />
-        )}
+        ) : (
+            <h1>Best Casino Offers for your country</h1>
+          )}
 
         {bonuses &&
-          country === "it" &&
           bonuses.length > 2 &&
           bonuses.map((bonus: StreamerBonus) => (
             <BonusStripe
@@ -135,7 +120,6 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
           ))}
 
         {bonuses &&
-          country === "it" &&
           bonuses.length <= 2 &&
           streamerData.bonuses.map((bonus: StreamerBonus) => (
             <BonusStripe
@@ -146,15 +130,13 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
           ))}
 
         <div style={{ padding: "1rem" }}>
-          <VideoDiscalimer />
+          {country === "it" && <VideoDiscalimer />}
         </div>
         {process.env.REFER === "true" && (
           <div className="bottom">
             <p style={{ textAlign: "center" }}>
               This service is provided by{" "}
-              <a href="https://www.topaffiliation.com">
-                Top Affiliation
-              </a>
+              <a href="https://www.topaffiliation.com">Top Affiliation</a>
             </p>
           </div>
         )}
@@ -169,7 +151,7 @@ export async function getServerSideProps({ query }) {
   const aquaClient = new AquaClient();
 
   const streamer = await axios.get(
-    `${configuration.api}/streamers/${configuration.streamerId}`,
+    `${configuration.api}/streamers/${configuration.streamerId}`
   );
 
   return {
